5b23f8263e72a9d768df1e7020d4d073c3bd67b2
insist <983107244@qq.com>
2020-09-10 11:28:54 +0800

    添加leyou-manage-web工程

---
+++ leyou-manage-web/src/pages/item/Category.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <!-- 加上  :treeDate="treeDate" 可以使url失效，不加载url返回的数据而是用本地的数据 -->
+        <v-tree url="/item/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  import {treeData} from "../../mockDB";
+  //数据模型和导入的值一样，可以省略不写，直接写treeData即可
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true,
+        treeData
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+      },
+      handleDelete(id) {
+        console.log("delete ... " + id)
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
